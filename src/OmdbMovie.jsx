b897fa9692797267fee4004f21ec7489bf25b77a
Mistaweng <emmanuelgyang.1822@gmail.com>
2024-01-22 01:51:35 +0100

    Added hosted backend

--- src/OmdbMovie.jsx
+++ src/OmdbMovie.jsx
@@ -15,7 +15,7 @@ const OmdbMovie = () => {
   const searchMovies = async () => {
     try {
       const response = await axios.get(
-        `https://localhost:7113/api/Movie/search?title=${searchTerm}`
+        `https://omdbmovie-backend.onrender.com/api/Movie/search?title=${searchTerm}`
       );
       setSearchResults(response.data.search);
       setMovies(response.data.search);
@@ -78,7 +78,7 @@ const OmdbMovie = () => {
                   <div className="container">
                     {movies &&
                       movies.map((movie) => (
-                        <Link key={movie.imdbID} to={`https://localhost:7113/api/Movie/${movie.imdbID}`}>
+                        <Link key={movie.imdbID} to={`https://omdbmovie-backend.onrender.com/api/Movie/${movie.imdbID}`}>
                           <MovieCard movie={movie} />
                         </Link>
                       ))}
@@ -104,7 +104,7 @@ const OmdbMovie = () => {
               </div>
             }
           />
-          <Route path="https://localhost:7113/api/Movie/:id" element={<MovieDetails />} />
+          <Route path="https://omdbmovie-backend.onrender.com/api/Movie/:id" element={<MovieDetails />} />
         </Routes>
       </div>
     </Router>
