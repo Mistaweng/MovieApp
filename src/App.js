c40841c55c0124f7fd919c6a19f522999076d5f4
Mistaweng <emmanuelgyang.1822@gmail.com>
2024-01-22 01:21:35 +0100

    Modified the App

--- src/App.js
+++ src/App.js
@@ -1,72 +1,13 @@
-import {useState, useEffect} from "react";  
-import MovieCard from "./MovieCard";
-
-
+import React from 'react';
 import './App.css';
-import SearchIcon from './search.svg';
-
-const API_URL = 'http://www.omdbapi.com?apikey=ed1e1192';
-
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
+import OmdbMovie from './OmdbMovie';
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
     
-    const searchMovies = async (title) => {
-        const response = await fetch(`$(API_URL)&s=${title}`);
-        const data = await response.json();
-
-        setMovies(data.Search);
-
-    }
-
-    useEffect(() => {
-        searchMovies(`Spiderman`);
-    }, []);
-
     return(
-        <div className="app">
-            <h1>MovieLand</h1>
-
-            <div className="search">
-                <input
-                placeholder="Search for movies"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-
-                />
-                <img 
-                src={SearchIcon}
-                alt="search"
-                onClick={() => searchMovies(searchTerm)}
-
-                />
-            </div>
-
-            {
-                movies?.length > 0
-                ? (
-                    <div className="container">
-                       {movies.map((movie) => (
-                        <MovieCard movie={movie} />
-                       ))}
-                    </div>
-                ) :
-                (
-                    <div className="empty">
-                        <h1>No movie found</h1> 
-                    </div>
-                )
-            }
-
-        </div>
+        <>
+        <OmdbMovie />
+        </>
     );
 }
 
